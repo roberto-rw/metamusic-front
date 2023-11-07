2e915cfecc3421a6976fec4c1f679f2c02da1252
Vastem <vastemxt@gmail.com>
2023-11-07 15:08:23 -0700

    feat: login component - login.html

---
+++ components/login/login.js
@@ -0,0 +1,18 @@
+const template = document.createElement('template')
+
+const html = await (await fetch('../assets/login.html')).text()
+template.innerHTML = html
+
+export class Login extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({ mode: 'open' })
+        shadow.appendChild(template.content.cloneNode(true))
+    }
+
+    connectedCallback() {
+
+    }
+}
+
+customElements.define('login-comp', Login)
