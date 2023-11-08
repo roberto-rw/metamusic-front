c0f96480fffd4418072afa91dada7e47f03920ff
Vastem <vastemxt@gmail.com>
2023-11-08 11:37:59 -0700

    feat: sidebar component

---
+++ components/sidebar/sidebar.js
@@ -0,0 +1,19 @@
+const template = document.createElement('template')
+
+const html = await (await fetch('../assets/sidebar.html')).text()
+template.innerHTML = html
+
+export class Sidebar extends HTMLElement {
+    constructor() {
+        super()
+        const shadow = this.attachShadow({ mode: 'open' })
+        shadow.appendChild(template.content.cloneNode(true))
+    }
+
+    connectedCallback() {
+
+    }
+
+}
+
+customElements.define('sidebar-comp', Sidebar)
