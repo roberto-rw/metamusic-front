afffe6afdd9ae788b985439efc0501028c8305c4
Roberto RW <robertorw7@gmail.com>
2023-11-11 13:26:40 -0800

    fix: merged homejs

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 import { Header } from "./components/header/header.js"
 import { Sidebar } from "./components/sidebar/sidebar.js"
 import { Player } from "./components/music-player/music-player.js"
-import { PlaylistCard } from "./components/playlist-card/playlist-card.js"
+import { PlaylistCard } from "./components/home/playlist-card.js"
 import { Searchbar } from "./components/searchbar/searchbar.js"
 import { SongCard } from "./components/song-card/song-card.js"
 import { HomeContent } from "./components/home/home-content.js"
