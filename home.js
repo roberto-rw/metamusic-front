b9c9e57c735f2c672fae4d9db8f2efc8a869b7ae
Vastem <vastemxt@gmail.com>
2023-11-10 22:29:38 -0700

    update: components size

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 import { Header } from "./components/header/header.js"
 import { Sidebar } from "./components/sidebar/sidebar.js"
 import { Player } from "./components/music-player/music-player.js"
+import { HomeContent } from "./components/home/home-content.js"
 import { PlaylistCard } from "./components/playlist-card/playlist-card.js"
