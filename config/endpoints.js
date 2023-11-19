f4fae1c4745239da9d2fb81a5bf8d193299125a2
Vastem <vastemxt@gmail.com>
2023-11-19 12:43:44 -0700

    fix: song request

--- config/endpoints.js
+++ config/endpoints.js
@@ -1,8 +1,7 @@
 export const ENDPOINTS = {
-    VITE_DEEZER_KEY: "39bdd77d46msh122e62fb042475cp1f49f2jsne18b20b00f24",
     API_URL: "http://localhost:3000/",
-    USER_URL : "http://localhost:3000/user/",
-    PLAYLIST_URL : "http://localhost:3000/playlist/",
-    SONG_URL : "http://localhost:3000/song/",
-    SUBSCRIPTION_URL : "http://localhost:3000/subscription/"
+    USER_URL: "http://localhost:3000/user/",
+    PLAYLIST_URL: "http://localhost:3000/playlist/",
+    SONG_URL: "http://localhost:3000/song/",
+    SUBSCRIPTION_URL: "http://localhost:3000/subscription/"
 };
--- service/songService.js
+++ service/songService.js
@@ -3,12 +3,13 @@ import { ENDPOINTS } from "../config/endpoints";
 export async function getSongsByName(name) {
     const options = {
         method: "GET",
+        credentials: 'include',
         headers: {
         },
     }
     let response = await fetch((ENDPOINTS.SONG_URL + "search/byname/" + name), options)
     let json = await response.json()
-    
+
     // Comprueba si json es un array. Si no lo es, devuelve un array vacío
     return Array.isArray(json) ? json : [];
 }
