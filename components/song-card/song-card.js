4d9bdb7faeba1b1f37fb27090aaef77529ecd4a9
Roberto RW <robertorw7@gmail.com>
2023-11-20 09:30:04 -0800

    feat: add togle play pause

--- components/song-card/song-card.js
+++ components/song-card/song-card.js
@@ -4,22 +4,29 @@ const html = await (await fetch('../assets/song-card.html')).text()
 template.innerHTML = html
 
 export class SongCard extends HTMLElement {
-    static get observedAttributes() {
-        return ['name', 'artist', 'duration'];
-    }
-
     constructor() {
         super()
         const shadow = this.attachShadow({ mode: 'open' })
         shadow.appendChild(template.content.cloneNode(true))
     }
 
-    attributeChangedCallback(name, oldValue, newValue) {
-        this.shadowRoot.getElementById(name).textContent = newValue;
+    connectedCallback() {
+        this.shadowRoot.querySelector('img').src = this.getAttribute('img');
+        this.shadowRoot.querySelector('#name').textContent = this.getAttribute('name');
+        this.shadowRoot.querySelector('#duration').textContent = this.getAttribute('duration');
+        this.shadowRoot.querySelector('#artist').textContent = this.getAttribute('artist');
+        
+        this.shadowRoot.querySelector('#img-container').addEventListener('click', this.togglePlayPause.bind(this));
     }
 
-    async connectCallback() {
-
+    // Cambia el icono de play y pausa
+    togglePlayPause() {
+        const playIcon = this.shadowRoot.querySelector('.play-icon');
+        if (playIcon.textContent.trim() === '▶') {
+            playIcon.textContent = '⏸';
+        } else {
+            playIcon.textContent = '▶';
+        }
     }
 }
 
