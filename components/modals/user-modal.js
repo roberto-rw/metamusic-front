3f0b66804e7f6c1ce8543b4e66ef1592d5dce4e7
Vastem <vastemxt@gmail.com>
2023-11-29 12:03:52 -0700

    update: toggle user menu

--- components/modals/user-modal.js
+++ components/modals/user-modal.js
@@ -4,20 +4,42 @@ template.innerHTML = html
 
 export class UserModal extends HTMLElement {
     #userMenu
+    #isOpen
+    #boundHandleClose
 
     constructor() {
         super()
         const shadow = this.attachShadow({ mode: 'open' })
         shadow.appendChild(template.content.cloneNode(true))
 
+        this.#isOpen = false
+        this.#boundHandleClose = this.handleClose.bind(this)
         this.#userMenu = this.shadowRoot.getElementById('user-modal')
     }
 
-    connectedCallback() {}
+    connectedCallback() {
+    }
 
     toggleUserMenu() {
         this.#userMenu.classList.toggle('hidden')
-        console.log('toggleUserMenu')
+        if (this.#isOpen) {
+            this.#isOpen = false
+            document.removeEventListener('click', this.#boundHandleClose)
+        } else {
+            this.#isOpen = true
+            setTimeout(() => {
+                document.addEventListener('click', this.#boundHandleClose)
+            }, 0)
+
+        }
+    }
+
+    handleClose() {
+        if (this.#isOpen) {
+            this.#userMenu.classList.add('hidden')
+            document.removeEventListener('click', this.#boundHandleClose)
+            this.#isOpen = false
+        }
     }
 }
 
